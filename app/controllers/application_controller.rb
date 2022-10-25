c10b24e157a14e21d8f5bb6ea9c4f46304e3f80c
William <wms274@nyu.edu>
2022-10-25 15:09:27 -0400

    form backend stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,31 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  
   get "/mygames/:id" do
     games = User.find(params[:id]).games
     games.to_json
   end
 
-  get "/games/:id" do
-    game = Games.find(params[:id])
+  get "/allgames/:id" do
+    game = Game.find(params[:id])
     game.to_json
+  end
+
+  patch "/allgames/:id" do
+    game = Game.find(params[:id])
+    game.update(position: params[:position])
+    game.to_json
+  end
+
+  get "/users" do
+    users = User.all
+    users.to_json
+  end
+
+  post "/users" do
+    user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    user.to_json
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_24_145035) do
+ActiveRecord::Schema.define(version: 2022_10_25_164200) do
 
   create_table "games", force: :cascade do |t|
     t.string "position"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2022_10_24_145035) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
+    t.string "password"
   end
 
 end
