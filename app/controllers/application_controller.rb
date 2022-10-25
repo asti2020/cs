824fa8e52bdf836cb85b55e16da56ad8e850b848
Astika Luintel <astild2020@gmail.com>
2022-10-25 12:39:47 -0400

    tuesday 1pm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,22 @@ class ApplicationController < Sinatra::Base
     game.to_json
   end
 
+  get "/game" do
+    games = Game.all
+    games.to_json
+  end
+
+
+  # get "/user/:id" do
+  #   users = User.find_for_authentication(email: params[:user][:email])
+  #   User.valid_password?(params[:user][:password])
+
+  #   users.to_json
+  # end
+
+  post "/users/:id" do
+    user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    user.to_json
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 puts "🌱 Seeding spices..."
 
-User.create(username: 'Will')
-User.create(username: 'Astika')
-User.create(username: 'John')
+User.create(username: 'Will', email: "will@example.com", password: '123')
+User.create(username: 'Astika', email: "Astika@example.com", password: '123')
+User.create(username: 'John', email: "John@example.com", password: '123')
 
 Game.create(position: 'r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 2 3')
 Game.create(position: 'rnbqkbnr/pppp1ppp/4p3/8/8/2N5/PPPPPPPP/R1BQKBNR b KQkq - 1 1')
