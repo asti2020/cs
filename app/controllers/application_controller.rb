cc8c8bc704b6e805d4b4535ad596ae3f87d099d1
Astika Luintel <astild2020@gmail.com>
2022-10-25 19:28:32 -0400

    before merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,22 +12,50 @@ class ApplicationController < Sinatra::Base
     game.to_json
   end
 
-  get "/game" do
+  get "/" do
     games = Game.all
     games.to_json
   end
 
+  get '/matchup' do
+    projects = Game.all
+    projects.to_json(include: [:users, :matchups])
+  end
 
-  # get "/user/:id" do
-  #   users = User.find_for_authentication(email: params[:user][:email])
-  #   User.valid_password?(params[:user][:password])
 
-  #   users.to_json
+  # get "/log" do
+  # #  if User.valid_password?(params[:user][:password])
+  # #   user = User.find_for_authentication(email: params[:user][:email])
+  # #   user.to_json
+  # #  else
+  # #   {message: "Invalid email or password"}.to_json
+  # #  end
+  #   # users = User.find_for_authentication(email: params[:user][:email])
+  #   # User.valid_password?(params[:user][:password])
+  #   # users.to_json
+  # end
+  # get "/log" do
+  #   users = User.where(user_id: new.id)
+  #   users.to_json(include: [:games, :matchups])
   # end
 
-  post "/users/:id" do
+
+  post "/users" do
     user = User.create(username: params[:username], email: params[:email], password: params[:password])
     user.to_json
+  end
+
+
+  patch "/users/:id" do
+    users = User.find(params[:id])
+    users.update(username: params[:username], email: params[:email], password: params[:password])
+    users.to_json
+  end
+
+  delete "/users/:id" do
+    users = User.find(params[:id])
+    users.destroy
+    users.to_json
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
     has_many :matchups
     has_many :games, through: :matchups
+
+    def userauthenticity
+        user = User.find_for_authentication(email: params[:user][:email])
+        User.valid_password?(params[:user][:password])
+    end
 end
