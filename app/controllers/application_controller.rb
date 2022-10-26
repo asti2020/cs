12aa212803ad89e3c8cb50d3b2531032bce0665e
Astika Luintel <astild2020@gmail.com>
2022-10-26 10:30:59 -0400

    before merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,41 @@ class ApplicationController < Sinatra::Base
     games.to_json
   end
 
+  get '/allgames' do
+    games = Game.all
+    games.to_json
+  end
+
+  get '/mygames' do
+    mygames = Game.all.select do |game|
+      game.users.include?(User.find(params[:user_id]))
+        mygames.to_json
+    end
+  end
+
   get "/games/:id" do
     game = Games.find(params[:id])
     game.to_json
+  end
+
+  get "/matchups/:id" do
+    matchups = User.find(params[:id]).matchups
+    matchups.to_json
+  end
+
+  get "/users/:id" do
+    user = User.find(params[:id])
+    user.to_json
+  end
+
+  get "/users" do
+    users = User.all
+    users.to_json
+  end
+
+  post "/users" do
+    user = User.create(params)
+    user.to_json
   end
 
 end
